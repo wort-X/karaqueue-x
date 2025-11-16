42c2a21348945ee322157487e04338b8f1a5af4f
Tim Fritzen <tim.fritzen@rwth-aachen.de>
2025-11-16 23:12:12 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <svelte:head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Karaque</title>
+    <title>Karaqueue</title>
     <link rel="icon" href={favicon} />
 </svelte:head>
 
